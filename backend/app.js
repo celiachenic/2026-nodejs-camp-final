27b71c0291e62f7fc4752618866ea8a36d2abfe9
msnigatsu <msnigatsu@gmail.com>
2026-08-25 14:42:11 +0800

    feat: add global error handler

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-const skillRouter = require('./routes/skillRouter')
-const packageRouter = require('./routes/packageRouter')
+const skillRouter = require("./routes/skillRouter");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 //路由
-app.get('/healthcheck',(req,res)=>{
-    return res.send('OK')
-})
+app.get("/healthcheck", (req, res) => {
+  return res.send("OK");
+});
 
-app.use('/api/coaches',skillRouter)
+app.use("/api/coaches", skillRouter);
 
-module.exports = app
+app.use((err, req, res, next) => {
+  return res.status(500).json({
+    status: "failed",
+    message: err.message,
+  });
+});
+module.exports = app;
