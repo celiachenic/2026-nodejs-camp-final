037034db60f9b46ae4b0a1d9bb576eb09ac4a313
msnigatsu <msnigatsu@gmail.com>
2026-09-07 20:22:08 +0800

    add course booking endpoint

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,7 +1,11 @@
 const createError = require("../utils/createError");
 const appDataSource = require("../db/dataSource");
 const courseSchema = require("../db/entities/Course");
-const { LessThanOrEqual, MoreThan } = require("typeorm");
+const bookingSchema = require("../db/entities/Booking");
+const { LessThanOrEqual, MoreThan, IsNull } = require("typeorm");
+const isUuid = require("../utils/isUuid");
+const getUserCredits = require("../services/creditService");
+const getUserBookings = require("../services/bookingService");
 
 const getCourses = async (req, res, next) => {
   try {
@@ -43,4 +47,59 @@ const getCourses = async (req, res, next) => {
     return next(createError(500, "取得進行中課程列表失敗"));
   }
 };
-module.exports = { getCourses };
+
+const bookCourse = async (req, res, next) => {
+  try {
+    const { courseId } = req.params;
+    const user = req.user;
+
+    if (!isUuid(courseId)) {
+      return next(createError(400, "ID錯誤"));
+    }
+
+    const { creditRemain } = await getUserCredits(user.id);
+    const bookings = await getUserBookings(user.id);
+
+    if (creditRemain === 0) {
+      return next(createError(400, "已無可使用堂數"));
+    }
+
+    const alreadyBooked = bookings.find(
+      (booking) => booking.course.id === courseId,
+    );
+    if (alreadyBooked) {
+      return next(createError(400, "已經報名過此課程"));
+    }
+
+    const courseRepo = appDataSource.getRepository(courseSchema);
+    const bookingRepo = appDataSource.getRepository(bookingSchema);
+    const courseCurrentParticipants = await bookingRepo.count({
+      where: { course: { id: courseId }, cancelled_at: IsNull() },
+    });
+    const course = await courseRepo.findOneBy({ id: courseId });
+    if (!course) {
+      return next(createError(400, "ID錯誤"));
+    }
+    if (course.max_participants === courseCurrentParticipants) {
+      return next(createError(400, "已達最大參加人數，無法參加"));
+    }
+
+    const newBooking = await bookingRepo.save({
+      user: { id: user.id },
+      course: { id: courseId },
+    });
+
+    if (!newBooking) {
+      return next(createError(500, "報名課程失敗"));
+    }
+
+    return res.status(201).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    console.log(error);
+    return next(createError(500, "報名課程失敗"));
+  }
+};
+module.exports = { getCourses, bookCourse };
--- backend/routes/courseRouter.js
+++ backend/routes/courseRouter.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middlewares/authMiddleware");
 const courseController = require("../controllers/courseController");
 router.get("/", courseController.getCourses);
+router.post("/:courseId", authMiddleware, courseController.bookCourse);
 module.exports = router;
